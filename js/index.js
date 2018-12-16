80060101da7da0f9a4ecf9ad85d808364b44dfbc
totopiahan <totopiahan@gmail.com>
2018-12-17 01:23:42 +0900

    메뉴 planet animation 으로 변경, 별자리 라인굵기 늘림

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var canvasDots = function() {
   canvas.height = window.innerHeight;
   canvas.style.display = 'block';
   ctx.fillStyle = colorDot;
-  ctx.lineWidth = .1;
+  ctx.lineWidth = .5;
   ctx.strokeStyle = color;
 
   var mousePosition = {
@@ -93,7 +93,7 @@ var canvasDots = function() {
   window.onmousemove = function(parameter) {
     mousePosition.x = parameter.pageX;
     mousePosition.y = parameter.pageY;
-  }
+  };
 
   mousePosition.x = window.innerWidth / 2;
   mousePosition.y = window.innerHeight / 2;
