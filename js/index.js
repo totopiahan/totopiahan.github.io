40d7e325adc3dddb1eabfe321c0f1650c5c482a7
Byungyoung Han <byhan@decenter.kr>
2019-12-07 05:54:54 +0900

    Change dot,line, title,hover colors

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var canvasDots = function() {
   var canvas = document.querySelector('canvas'),
       ctx = canvas.getContext('2d'),
-      colorDot = '#00bdbf',
-      color = '#00bdbf';
+      colorDot = '#00FF41',
+      color = '#00FF41';
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   canvas.style.display = 'block';
